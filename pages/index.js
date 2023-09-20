4e548083cfab567ecfb76804ec21835f1e6ba5d1
robindfernandes <113320356+robindfernandes@users.noreply.github.com>
2023-09-20 15:03:01 +1000

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -10,13 +10,12 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <main>
-        <h1 className="title" style={{ fontSize: '24px' }}>Welcome to RealmHR Services</h1>
-        <Header title="Welcome to RealmHR Services" /> {/* Pass the title prop here */}
+        <Header></><h1 className="title" style={{ fontSize: '24px' }}>Welcome to RealmHR Services</h1> {/* Use the standalone h1 element */}
         <img
           src="/logo-realm.png"
           alt="RealmHR Services Logo"
           className="logo"
-          style={{ width: '200px', height: 'auto' }} // Adjust the width as needed
+          style={{ width: '200px', height: 'auto' }}
         />
         <p className="description">
           RealmHR Services is your trusted partner in recruitment, specializing in the retail, FMCG, and ITES sectors. With our deep industry knowledge and a commitment to excellence, we connect businesses with top-tier talent, helping them thrive and grow. In the dynamic world of retail, FMCG, and ITES, finding the right talent is crucial for success. At RealmHR Services, we understand the unique challenges and demands of these sectors. Our dedicated team of recruiters has a proven track record of identifying and placing exceptional professionals who can drive your business forward.
