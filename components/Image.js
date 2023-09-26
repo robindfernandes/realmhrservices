6b792601d32dd0be7b0f90f4fc42ce1c55c72ce4
robindfernandes <113320356+robindfernandes@users.noreply.github.com>
2023-09-26 11:23:44 +1000

    made indexpage.js and imported pics

---
+++ components/Image.js
@@ -0,0 +1,42 @@
+// indexpage.js
+import Image from 'next/image';
+import Image1 from '../public/images/RealmHR1.jpg';
+import Image2 from '../public/images/RealmHR2.jpg';
+import Image3 from '../public/images/RealmHR3.jpg';
+
+const IndexPage = () => {
+  return (
+    <div>
+      <h1>We are RealmHR Services</h1>
+      <div>
+        <Image
+          src={Image1}
+          alt="Image 1"
+          width={400} // Set the desired width
+          height={300} // Set the desired height
+        />
+        <p>Csutomer Delight is in our DNA</p>
+      </div>
+      <div>
+        <Image
+          src={Image2}
+          alt="Image 2"
+          width={400}
+          height={300}
+        />
+        <p>Personal Touch</p>
+      </div>
+    <div>
+     <Image
+     src={Image3}
+     alt="Image 3"
+     width={400}
+     height={300}
+   />
+   <p>With you at every step</p>
+ </div>
+</div>
+  );
+};
+
+export default IndexPage;
