d575218f2b758f02a4321c9ac1d9d0012bac168b
robindfernandes <113320356+robindfernandes@users.noreply.github.com>
2023-09-26 14:23:08 +1000

    Update IndexPage.js

--- pages/IndexPage.js
+++ pages/IndexPage.js
@@ -1,41 +1,41 @@
-// IndexPage.js
-
 import React from 'react';
 import Image from 'next/image';
 
 const IndexPage = () => {
-    console.log("Rendering IndexPage"); // Add this line
-      return (
-        <div>
-          <h1>We are RealmHR Services</h1>
-          <div>
-          <Image
-      src="/realm1.png"
-      alt="Image 1"
-      className="image" // Add your CSS class here
-      style={{ width: '200px', height: 'auto' }}
-    />
-            <p>Customer Delight is in our DNA</p>
-          </div>
-          <div>
-          <Image
-      src="/realm2.png"
-      alt="Image 1"
-      className="image" // Add your CSS class here
-      style={{ width: '200px', height: 'auto' }}
-    />
-            <p>Personal Touch</p>
-          </div>
-          <div>
-          <Image
-      src="/realm3.png"
-      alt="Image 1"
-      className="image" // Add your CSS class here
-      style={{ width: '200px', height: 'auto' }}
-    />
-            <p>With you at every step</p>
-          </div>
-        </div>
-      );
-    };
-    export { IndexPage };
+  console.log("Rendering IndexPage");
+
+  return (
+    <div>
+      <h1>We are RealmHR Services</h1>
+      <div>
+        <Image
+          src="/realm1.png"
+          alt="Image 1"
+          className="image" // Add your CSS class here
+          style={{ width: '200px', height: 'auto' }}
+        />
+        <p>Customer Delight is in our DNA</p>
+      </div>
+      <div>
+        <Image
+          src="/realm2.png"
+          alt="Image 2"
+          className="image" // Add your CSS class here
+          style={{ width: '200px', height: 'auto' }}
+        />
+        <p>Personal Touch</p>
+      </div>
+      <div>
+        <Image
+          src="/realm3.png"
+          alt="Image 3"
+          className="image" // Add your CSS class here
+          style={{ width: '200px', height: 'auto' }}
+        />
+        <p>With you at every step</p>
+      </div>
+    </div>
+  );
+};
+
+export default IndexPage;
