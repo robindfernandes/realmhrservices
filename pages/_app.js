70cf83d06e40690b184d89c3428a1236d6fdb01c
robindfernandes <113320356+robindfernandes@users.noreply.github.com>
2023-09-27 15:51:34 +1000

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,6 @@
 import '@styles/globals.css'; // Import your global CSS
 import { useEffect } from 'react';
+import Head from 'next/head'; // Import the Head component
 
 function MyApp({ Component, pageProps }) {
   // Add any global JavaScript or effects here
@@ -7,7 +8,15 @@ function MyApp({ Component, pageProps }) {
     // Global JavaScript or effects can go here
   }, []);
 
-  return <Component {...pageProps} />;
+  return (
+    <>
+      {/* Add the viewport meta tag */}
+      <Head>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+      </Head>
+      <Component {...pageProps} />
+    </>
+  );
 }
 
 export default MyApp;
