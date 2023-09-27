0c6aae3324f496da9f5ac82989d3ffe5be058e8e
robindfernandes <113320356+robindfernandes@users.noreply.github.com>
2023-09-27 12:48:24 +1000

    Update clients-testimonials.js

--- pages/clients-testimonials.js
+++ pages/clients-testimonials.js
@@ -4,48 +4,76 @@ import React from 'react';
 
 const ClientsTestimonials = () => {
   return (
-    <div className="clients-testimonials">
-      <h1>What Our Clients say about RealmHR Services:</h1>
-      <h2>1. National Group Of Companies - India</h2>
-      <p>
-      <strong>"RealmHR Services has been an invaluable partner in our recruitment efforts. 
-       Their efficiency and dedication to understanding our unique needs in the Retail and FMCG sectors have resulted in exceptional hires that have greatly contributed to our company's growth.
-       Jaykumar Mistry - Chief Marketing Officer(National Group Of Companies)" </strong>
-      </p>
-
-      {/*<h2>Our Expertise:</h2>
-      <p>
-        <strong>ITES (Information Technology Enabled Services):</strong> We excel in finding top talent for the ITES sector, ensuring that our clients have the skilled professionals they need to thrive in today's fast-paced tech-driven environment.
-      </p>
-      <p>
-        <strong>FMCG (Fast-Moving Consumer Goods):</strong> We understand the unique demands of the FMCG industry and connect companies with dynamic individuals who can drive growth and success.
-      </p>
-      <p>
-        <strong>Retail:</strong> Our comprehensive knowledge of the retail sector allows us to source candidates who can elevate the customer experience and contribute to retail excellence.
-      </p>
-
-      <h2>Discover the RealmHR Services Advantage</h2>
-      <ul>
-        <li><strong>Proven Track Record:</strong> With a wealth of experience, we have successfully placed candidates in key positions across various industries.</li>
-        <li><strong>Tailored Solutions:</strong> We offer customized recruitment solutions to meet the unique needs of each client, ensuring the right fit for every role.</li>
-        <li><strong>Exceptional Talent Pool:</strong> Our extensive network and rigorous selection process enable us to provide access to top-tier talent.</li>
-        <li><strong>Client-Centric Approach:</strong> We prioritize understanding our clients' goals and challenges to deliver results that drive their success.</li>
-  </ul> */}
-
-      <h2>Contact Us:</h2>
-      <p>
-        Email: <a href="mailto:teejay@realmhrservices.com">teejay@realmhrservices.com</a>
-      </p>
-      <p>
-        Phone: +91 93727 87542
-      </p>
-      <p>
-        Website: <a href="https://www.realmhrservices.com">www.realmhrservices.com</a>
-      </p>
-      <p>
-        Connect with us on LinkedIn: <a href="https://www.linkedin.com/company/realmhrservices">Follow us on LinkedIn</a>
-      </p>
+    <><div className="logo-container">
+      <img
+        src="/logo-realm.png"
+        alt="RealmHR Services Logo"
+        className="logo" />
     </div>
+    <div className="clients-testimonials">
+        <h1>What Our Clients say about RealmHR Services:</h1>
+        <h2>1. National Group Of Companies - India</h2>
+        <p>
+          <strong>"RealmHR Services has been an invaluable partner in our recruitment efforts.
+            Their efficiency and dedication to understanding our unique needs in the Retail and FMCG sectors
+            have resulted in exceptional hires that have greatly contributed to our company's growth."</strong>
+        </p>
+        <p><strong>Jaykumar Mistry - Chief Marketing Officer</strong>
+          <p />
+        </p>
+
+        <h2>2. RDF Group Of Companies</h2>
+        <p>
+          <strong>"Working with RealmHR Services has been a game-changer for us in the Media industry.
+            Their ability to quickly source top-tier talent, coupled with their industry knowledge, has made a significant impact on our team's productivity and success."</strong>
+        </p>
+        <p><strong>Ronald D Feepling - National HR Head</strong>
+          <p />
+        </p>
+
+        <h2>3. MKR Group Of Companies</h2>
+        <p>
+          <strong>"We can't thank RealmHR Services enough for their outstanding support in the Pharma industry.
+            Their personalized and tailored recruitment solutions have consistently provided us with skilled professionals who align perfectly with our company's goals.
+            They are our go-to partner for talent acquisition."</strong>
+        </p>
+        <p><strong>Max K Rowling - National HR Head</strong>
+          <p />
+        </p>
+
+        {/*<h2>Our Expertise:</h2>
+    <p>
+      <strong>ITES (Information Technology Enabled Services):</strong> We excel in finding top talent for the ITES sector, ensuring that our clients have the skilled professionals they need to thrive in today's fast-paced tech-driven environment.
+    </p>
+    <p>
+      <strong>FMCG (Fast-Moving Consumer Goods):</strong> We understand the unique demands of the FMCG industry and connect companies with dynamic individuals who can drive growth and success.
+    </p>
+    <p>
+      <strong>Retail:</strong> Our comprehensive knowledge of the retail sector allows us to source candidates who can elevate the customer experience and contribute to retail excellence.
+    </p>
+
+    <h2>Discover the RealmHR Services Advantage</h2>
+    <ul>
+      <li><strong>Proven Track Record:</strong> With a wealth of experience, we have successfully placed candidates in key positions across various industries.</li>
+      <li><strong>Tailored Solutions:</strong> We offer customized recruitment solutions to meet the unique needs of each client, ensuring the right fit for every role.</li>
+      <li><strong>Exceptional Talent Pool:</strong> Our extensive network and rigorous selection process enable us to provide access to top-tier talent.</li>
+      <li><strong>Client-Centric Approach:</strong> We prioritize understanding our clients' goals and challenges to deliver results that drive their success.</li>
+</ul> */}
+
+        <h2>Contact Us:</h2>
+        <p>
+          Email: <a href="mailto:teejay@realmhrservices.com">teejay@realmhrservices.com</a>
+        </p>
+        <p>
+          Phone: +91 93727 87542
+        </p>
+        <p>
+          Website: <a href="https://www.realmhrservices.com">www.realmhrservices.com</a>
+        </p>
+        <p>
+          Connect with us on LinkedIn: <a href="https://www.linkedin.com/company/realmhrservices">Follow us on LinkedIn</a>
+        </p>
+      </div></>
   );
 }
 
