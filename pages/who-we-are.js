74577224a09cc5564fd933fb48b0d1adf881ecdc
robindfernandes <113320356+robindfernandes@users.noreply.github.com>
2023-09-28 10:44:23 +1000

    updated img and format

--- pages/who-we-are.js
+++ pages/who-we-are.js
@@ -17,7 +17,7 @@ const WhoWeAre = () => {
       <div className={styles.container}>
         <Header title="&ldquo;Founders Profile&rdquo;" />
         <img
-          src="/2.png"
+          src="/2.JPG"
           alt="Image 2"
           className="image"
           style={{ width: '50%', height: 'auto' }}
@@ -25,13 +25,14 @@ const WhoWeAre = () => {
         <div className="who-we-are">
           <h1 className={styles.title}>TEJASWEETA MISTRY</h1>
           <h2><strong>Founder | REALMHR Services</strong></h2>
-          <p>
-          Tejasweeta Mistry, a name embodying majesty, dignity, and an unwavering wellspring of positive energy, stands as a luminary in the realm of recruitment and human resources. With an illustrious career spanning over 15 years, Tejasweeta has honed her skills and acumen in the art of managing organizational human capital, making her a formidable force in the industry.
-          Her journey to becoming a recruitment industry expert is marked by a relentless pursuit of knowledge and hands-on experience within distinguished corporations, coupled with impactful consulting engagements. Tejasweeta's extensive knowledge traverses the entire spectrum of HR functions, encompassing Talent Acquisition, Employee Life Cycle Management, Performance Management, Compensation & Benefits, Payroll & Compliance, HR Audit, and HRIS Management. Her unwavering commitment to compliance and her profound understanding of regulatory and legal frameworks are the cornerstones of her success.
-          Armed with a Master's Degree from the prestigious Narsee Monjee Institute of Management Studies, Tejasweeta has ascended to the status of a true subject matter expert in HR Shared Services. Her extensive skill set and hands-on experience empower her to provide strategic guidance to senior management, particularly in matters pertaining to human capital. Tejasweeta's remarkable achievements include spearheading organizational structure changes, spearheading the seamless implementation of cutting-edge HRIS systems, and adeptly navigating the intricate HR landscape in high-stakes Merger & Acquisition projects.
-          In the dynamic world of recruitment, Tejasweeta Mistry shines as an unwavering beacon of expertise, innovation, and excellence, guiding organizations toward a brighter future.
-          </p>
-
+          <p>Tejasweeta Mistry, a name embodying majesty, dignity, and an unwavering wellspring of positive energy, stands as a luminary in the realm of recruitment and human resources. With an illustrious career spanning over 15 years, Tejasweeta has honed her skills and acumen in the art of managing organizational human capital, making her a formidable force in the industry.</p>
+          
+          <p>Her journey to becoming a recruitment industry expert is marked by a relentless pursuit of knowledge and hands-on experience within distinguished corporations, coupled with impactful consulting engagements. Tejasweeta's extensive knowledge traverses the entire spectrum of HR functions, encompassing Talent Acquisition, Employee Life Cycle Management, Performance Management, Compensation & Benefits, Payroll & Compliance, HR Audit, and HRIS Management. Her unwavering commitment to compliance and her profound understanding of regulatory and legal frameworks are the cornerstones of her success.</p>
+          
+          <p>Armed with a Master's Degree from the prestigious Narsee Monjee Institute of Management Studies, Tejasweeta has ascended to the status of a true subject matter expert in HR Shared Services. Her extensive skill set and hands-on experience empower her to provide strategic guidance to senior management, particularly in matters pertaining to human capital. Tejasweeta's remarkable achievements include spearheading organizational structure changes, spearheading the seamless implementation of cutting-edge HRIS systems, and adeptly navigating the intricate HR landscape in high-stakes Merger & Acquisition projects.</p>
+          
+          <p>In the dynamic world of recruitment, Tejasweeta Mistry shines as an unwavering beacon of expertise, innovation, and excellence, guiding organizations toward a brighter future.</p>
+        
         <h2><strong>Contact Us:</strong></h2>
         <p>
           Email: <a href="mailto:teejay@realmhrservices.com"><strong>teejay@realmhrservices.com</strong></a>
