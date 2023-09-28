3bb969b568100fed1b8f492f39b9ab5d82231d16
robindfernandes <113320356+robindfernandes@users.noreply.github.com>
2023-09-28 11:54:25 +1000

    3

--- pages/clients-testimonials.js
+++ pages/clients-testimonials.js
@@ -1,76 +1,76 @@
-// clients-testimonials.js
-
 import React from 'react';
 import Head from 'next/head';
 import Header from '@components/Header';
 import Footer from '@components/Footer';
+import styles from './clients-testimonials.module.css'; // Import the CSS module for page-specific styles
 
 const ClientsTestimonials = () => {
-  return (
-    <>
-      <Head>
-        <title>Clients Testimonials</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
+    return (
+        <>
+            <Head>
+                <title>Clients Testimonials</title>
+                <link rel="icon" href="/favicon.ico" />
+            </Head>
 
-      <div className="container"> {/* Use global CSS class here */}
-        <Header title= "Clients Testimonials" 
-        img
-          src="/logo-realm.png"
-          alt="RealmHR Services Logo"
-          className="logo" /* Use global CSS class here */
-          style={{ width: '50px', height: 'auto' }} 
-        />
-      </div>
-      <div className="clients-testimonials"> {/* Use global CSS class here */}
+            <div className={styles.container}>
+                {/* Use CSS Modules class here */}
+                <Header title="&ldquo;Founders Profile&rdquo;" />
+                <img
+                    src="/logo-realm.png"
+                    alt="RealmHR Services Logo"
+                    className={styles.logo} // Use CSS Modules class here
+                    style={{ width: '50px', height: 'auto' }}
+                />
+            </div>
+            <div className={styles['clients-testimonials']}>
+                {/* Use CSS Modules class here */}
+                <h1>What Our Clients say about RealmHR Services:</h1>
+                <h2>1. National Group Of Companies - India</h2>
+                <p>
+                    <strong>"RealmHR Services has been an invaluable partner in our recruitment efforts.
+                        Their efficiency and dedication to understanding our unique needs in the Retail and FMCG sectors
+                        have resulted in exceptional hires that have greatly contributed to our company's growth."</strong>
+                </p>
+                <p><strong>Jaykumar Mistry - Chief Marketing Officer</strong>
+                    <p />
+                </p>
 
-        <h1>What Our Clients say about RealmHR Services:</h1>
-        <h2>1. National Group Of Companies - India</h2>
-        <p>
-          <strong>"RealmHR Services has been an invaluable partner in our recruitment efforts.
-            Their efficiency and dedication to understanding our unique needs in the Retail and FMCG sectors
-            have resulted in exceptional hires that have greatly contributed to our company's growth."</strong>
-        </p>
-        <p><strong>Jaykumar Mistry - Chief Marketing Officer</strong>
-          <p />
-        </p>
+                <h2>2. RDF Group Of Companies</h2>
+                <p>
+                    <strong>"Working with RealmHR Services has been a game-changer for us in the Media industry.
+                        Their ability to quickly source top-tier talent, coupled with their industry knowledge, has made a significant impact on our team's productivity and success."</strong>
+                </p>
+                <p><strong>Ronald D Feepling - National HR Head</strong>
+                    <p />
+                </p>
 
-        <h2>2. RDF Group Of Companies</h2>
-        <p>
-          <strong>"Working with RealmHR Services has been a game-changer for us in the Media industry.
-            Their ability to quickly source top-tier talent, coupled with their industry knowledge, has made a significant impact on our team's productivity and success."</strong>
-        </p>
-        <p><strong>Ronald D Feepling - National HR Head</strong>
-          <p />
-        </p>
+                <h2>3. MKR Group Of Companies</h2>
+                <p>
+                    <strong>"We can't thank RealmHR Services enough for their outstanding support in the Pharma industry.
+                        Their personalized and tailored recruitment solutions have consistently provided us with skilled professionals who align perfectly with our company's goals.
+                        They are our go-to partner for talent acquisition."</strong>
+                </p>
+                <p><strong>Max K Rowling - National HR Head</strong>
+                    <p />
+                </p>
 
-        <h2>3. MKR Group Of Companies</h2>
-        <p>
-          <strong>"We can't thank RealmHR Services enough for their outstanding support in the Pharma industry.
-            Their personalized and tailored recruitment solutions have consistently provided us with skilled professionals who align perfectly with our company's goals.
-            They are our go-to partner for talent acquisition."</strong>
-        </p>
-        <p><strong>Max K Rowling - National HR Head</strong>
-          <p />
-        </p>
-
-        <h2>Contact Us:</h2>
-        <p>
-          Email: <a href="mailto:teejay@realmhrservices.com">teejay@realmhrservices.com</a>
-        </p>
-        <p>
-          Phone: +91 93727 87542
-        </p>
-        <p>
-          Website: <a href="https://www.realmhrservices.com">www.realmhrservices.com</a>
-        </p>
-        <p>
-          Connect with us on LinkedIn: <a href="https://www.linkedin.com/company/realmhrservices">Follow us on LinkedIn</a>
-        </p>
-      </div>
-      <Footer />
-      </>
-  );
+                <h2>Contact Us:</h2>
+                <p>
+                    Email: <a href="mailto:teejay@realmhrservices.com">teejay@realmhrservices.com</a>
+                </p>
+                <p>
+                    Phone: +91 93727 87542
+                </p>
+                <p>
+                    Website: <a href="https://www.realmhrservices.com">www.realmhrservices.com</a>
+                </p>
+                <p>
+                    Connect with us on LinkedIn: <a href="https://www.linkedin.com/company/realmhrservices">Follow us on LinkedIn</a>
+                </p>
+            </div>
+            <Footer />
+        </>
+    );
 }
 
 export default ClientsTestimonials;
