907e420758d03aa30418710beb73e5f28cef0865
robindfernandes <113320356+robindfernandes@users.noreply.github.com>
2023-09-28 11:18:21 +1000

    e

--- pages/clients-testimonials.js
+++ pages/clients-testimonials.js
@@ -2,19 +2,25 @@
 
 import React from 'react';
 import Head from 'next/head';
+import styles from './clients-testimonials.css'; // Import the CSS module for page-specific styles
+import Header from '@components/Header';
+import Footer from '@components/Footer';
 
 const ClientsTestimonials = () => {
   return (
-    <><div className="logo-container logo-top-left">
+    <>
       <Head>
-        <title>Next.js Starter!</title>
+        <title>Clients Testimonials</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
+
+      <div className={styles.container}>
+        <Header title="&ldquo;Founders Profile&rdquo;" />
     <img
       src="/logo-realm.png"
       alt="RealmHR Services Logo"
       className="logo"
-      style={{ width: '200px', height: 'auto' }}
+      style={{ width: '20px', height: 'auto' }}
     />
   </div>
     <div className="clients-testimonials">
@@ -48,25 +54,6 @@ const ClientsTestimonials = () => {
           <p />
         </p>
 
-        {/*<h2>Our Expertise:</h2>
-    <p>
-      <strong>ITES (Information Technology Enabled Services):</strong> We excel in finding top talent for the ITES sector, ensuring that our clients have the skilled professionals they need to thrive in today's fast-paced tech-driven environment.
-    </p>
-    <p>
-      <strong>FMCG (Fast-Moving Consumer Goods):</strong> We understand the unique demands of the FMCG industry and connect companies with dynamic individuals who can drive growth and success.
-    </p>
-    <p>
-      <strong>Retail:</strong> Our comprehensive knowledge of the retail sector allows us to source candidates who can elevate the customer experience and contribute to retail excellence.
-    </p>
-
-    <h2>Discover the RealmHR Services Advantage</h2>
-    <ul>
-      <li><strong>Proven Track Record:</strong> With a wealth of experience, we have successfully placed candidates in key positions across various industries.</li>
-      <li><strong>Tailored Solutions:</strong> We offer customized recruitment solutions to meet the unique needs of each client, ensuring the right fit for every role.</li>
-      <li><strong>Exceptional Talent Pool:</strong> Our extensive network and rigorous selection process enable us to provide access to top-tier talent.</li>
-      <li><strong>Client-Centric Approach:</strong> We prioritize understanding our clients' goals and challenges to deliver results that drive their success.</li>
-</ul> */}
-
         <h2>Contact Us:</h2>
         <p>
           Email: <a href="mailto:teejay@realmhrservices.com">teejay@realmhrservices.com</a>
@@ -80,7 +67,9 @@ const ClientsTestimonials = () => {
         <p>
           Connect with us on LinkedIn: <a href="https://www.linkedin.com/company/realmhrservices">Follow us on LinkedIn</a>
         </p>
-      </div></>
+      </div>
+      <Footer />
+      </>
   );
 }
 
