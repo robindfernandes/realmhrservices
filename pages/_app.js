9fa588e96a49ab01e31927c7c56b920f181b092e
robindfernandes <113320356+robindfernandes@users.noreply.github.com>
2023-09-28 16:11:06 +1000

    g

--- pages/_app.js
+++ pages/_app.js
@@ -2,33 +2,34 @@ import '../styles/globals.css'; // Adjust the path as needed
 import Head from 'next/head';
 import Header from '@components/Header';
 import Footer from '@components/Footer';
-//import { useEffect } from 'react';
+import { useEffect } from 'react';
 
+function MyApp({ Component, pageProps }) {
   // Add any global JavaScript or effects here
-  // useEffect(() => { 
+  useEffect(() => {
     // Global JavaScript or effects can go here
-  //}, []);
-  function MyApp({ Component, pageProps }) {
-    return (
-      <div className="container">
-        <Head>
-          <title>Next.js Starter!</title>
-          <link rel="icon" href="/favicon.ico" />
-        </Head>
-        {/* Rest of your JSX */}
-        <Header>
-          <div className="logoContainer">
-            <img
-              src="/logo-realm.png"
-              alt="RealmHR Services Logo"
-              className="logo"
-            />
-            <h1 className="title">Welcome to RealmHR Services</h1>
-          </div>
-        </Header>
+  }, []);
+
+  return (
+    <div className="container">
+      <Head>
+        <title>Next.js Starter!</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      {/* Rest of your JSX */}
+      <Header>
+        <div className="logoContainer">
+          <img
+            src="/logo-realm.png"
+            alt="RealmHR Services Logo"
+            className="logo"
+          />
+          <h1 className="title">Welcome to RealmHR Services</h1>
+        </div>
+      </Header>
       <Component {...pageProps} />
       <Footer /> 
-      </div>
+    </div>
   );
 }
 
