61adde760ae4428eadb6e6c34bf46e6de5a8c65c
robindfernandes <113320356+robindfernandes@users.noreply.github.com>
2023-10-11 11:28:12 +1100

    added div blocks

--- components/Footer.js
+++ components/Footer.js
@@ -4,17 +4,16 @@ import styles from './Footer.module.css'
 export default function Footer() {
   return (
     <footer className={styles.footer}>
-      <a>
+      <div className={styles.logoContainer}>
+      <a>      
         <img src="/logo-realm.png" alt="Netlify Logo" className={styles.logo} />
       </a>
-      <p>
+      </div>
       <div className={styles.linkedinContainer}>
         <a href="https://www.linkedin.com/company/realmhrservices">
           <img src="/LinkedIn.png" alt="LinkedIn Logo" className={styles.LinkedInlogo} />
         </a>
         </div>
-      </p>
-   
     </footer>
-  )
+  );
 }
