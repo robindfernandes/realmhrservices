9b85fd14624f19e7148421496405897353e27469
robindfernandes <113320356+robindfernandes@users.noreply.github.com>
2023-10-11 11:19:20 +1100

    Update Footer.js

--- components/Footer.js
+++ components/Footer.js
@@ -4,14 +4,17 @@ import styles from './Footer.module.css'
 export default function Footer() {
   return (
     <footer className={styles.footer}>
-      <a href="https://www.linkedin.com/company/realmhrservices">
+      <a>
         <img src="/logo-realm.png" alt="Netlify Logo" className={styles.logo} />
       </a>
+      <p>
       <div className={styles.linkedinContainer}>
         <a href="https://www.linkedin.com/company/realmhrservices">
           <img src="/LinkedIn.png" alt="LinkedIn Logo" className={styles.LinkedInlogo} />
         </a>
-      </div>
+        </div>
+      </p>
+   
     </footer>
   )
 }
