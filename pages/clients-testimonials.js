f3b776a3453e8f14913eae9b8cb5f3efa3161dd3
robindfernandes <113320356+robindfernandes@users.noreply.github.com>
2023-10-12 16:30:30 +1100

    e

--- pages/clients-testimonials.js
+++ pages/clients-testimonials.js
@@ -32,13 +32,13 @@ return (
         <div className="button-container">
           {isMenuVisible && (
             <>
-            <Link href="/index"><button className="menu-link-button"><strong>Home</strong></button></Link>
+           {/*} <Link href="/index"><button className="menu-link-button"><strong>Home</strong></button></Link>
             <Link href="/who-we-are"><button className="menu-link-button"><strong>Who we are</strong></button>
             </Link>
             <Link href="/clients-testimonials"><button className="menu-link-button"><strong>Clients Testimonials</strong></button>
               </Link>
               <Link href="/Talk-to-us"><button className="menu-link-button"><strong>Talk to us</strong></button>
-                  </Link>
+          </Link> */}
             </>
             )}
         </div>  
--- pages/who-we-are.js
+++ pages/who-we-are.js
@@ -25,13 +25,13 @@ const WhoWeAre = () => {
         <div className="button-container">
           {isMenuVisible && (
             <>
-            <Link href="/index"><button className="menu-link-button"><strong>Home</strong></button></Link>
+           {/* <Link href="/index"><button className="menu-link-button"><strong>Home</strong></button></Link>
             <Link href="/who-we-are"><button className="menu-link-button"><strong>Who we are</strong></button>
             </Link>
             <Link href="/clients-testimonials"><button className="menu-link-button"><strong>Clients Testimonials</strong></button>
               </Link>
               <Link href="/Talk-to-us"><button className="menu-link-button"><strong>Talk to us</strong></button>
-                  </Link>
+          </Link> */}
             </>
             )}
         </div>  
